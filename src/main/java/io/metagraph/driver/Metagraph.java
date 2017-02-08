ba099cfaf8d75fa0342b36f07b0cf7d967942fcb
zhaoliang <weston_contribute@163.com>
2017-02-08 12:03:35 +0800

    ignore /target file

---
+++ src/main/java/io/metagraph/driver/Metagraph.java
@@ -0,0 +1,7 @@
+package io.metagraph.driver;
+
+/**
+ * Created by zhaoliang on 17-2-7.
+ */
+public class Metagraph {
+}
---
+++ src/main/java/io/metagraph/driver/Strategies.java
@@ -0,0 +1,7 @@
+package io.metagraph.driver;
+
+/**
+ * Created by (zhaoliang@metagraph.io) on (17-2-7).
+ */
+public enum Strategies {
+}
